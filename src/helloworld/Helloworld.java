46542ea7b69cbd3179b1a017a43e4805cffd650c
lyon515520 <lyon515520@gmail.com>
2014-07-16 22:01:07 +0200

    first commit
    hello world

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.print("holle world!");//this is the first projet of my java exercise
+	}
+
+}
